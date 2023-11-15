307df3fb8958b11c0344752824336d4f8bee026d
Anirudh Agarwal <anirudha58@gmail.com>
2023-11-15 13:32:16 +0530

    CORS Update

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -1,12 +1,6 @@
 exports.handler = async function (event, context) {
   const response = await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING",
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        // Add any other headers if needed
-      },
-    }
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
   );
 
   const data = await response.json();
