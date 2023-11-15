66c279d88603606b1ab3293f5ef9d905d90f7189
Anirudh Agarwal <anirudha58@gmail.com>
2023-11-15 13:35:05 +0530

    CORS update

--- functions/fetchData.js
+++ functions/fetchData.js
@@ -3,6 +3,15 @@ exports.handler = async function (event, context) {
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
   );
 
+  if (!response.ok) {
+    // Handle error response (non-JSON)
+    console.error(`Error: ${response.statusText}`);
+    return {
+      statusCode: response.status,
+      body: JSON.stringify({ error: response.statusText }),
+    };
+  }
+
   const data = await response.json();
 
   return {
